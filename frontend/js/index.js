e1b79544e0a6f131211e6d9d65e3eaddb86ad20a
Lexx Xell <lexxxell007@gmail.com>
2023-01-30 03:55:18 +0400

    Light frontend added

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -12,20 +12,55 @@ const ethEnable = async () => {
 };
 
 const initialize = () => {
+  let symbol = '';
+
   const buttonEthEnable = document.getElementById('button-eth-enable');
+  const buttonMyBalance = document.getElementById('button-contract-myBalance');
   const labelEthAccount = document.getElementById('label-eth-account');
+  const labelMyBalance = document.getElementById('label-contract-myBalance');
   const inputPdAddress = document.getElementById('input-pd-address');
+
+  const buttonContractOwner = document.getElementById('button-contract-owner');
+  const labelContractOwner = document.getElementById('label-contract-owner');
+
+  const inputContractBalanceOf = document.getElementById('input-contract-balanceOf');
+  const buttonContractBalanceOf = document.getElementById('button-contract-balanceOf');
+  const labelContractBalanceOf = document.getElementById('label-contract-balanceOf');
 
   inputPdAddress.value = config.contractAddress;
 
   buttonEthEnable.addEventListener('click', () => {
     ethEnable().then((result) => {
-      labelEthAccount.textContent = result ? window.accounts[0] : 'Can`t connect';
+      labelEthAccount.textContent = result ? 'Address: ' + window.accounts[0] : 'Can`t connect';
       buttonEthEnable.disabled = result;
+      buttonEthEnable.textContent = 'CONNECTED';
       if (result) {
         window.profitDivider = new window.web3.eth.Contract(config.abi, inputPdAddress.value);
+        window.profitDivider.methods
+          .symbol()
+          .call()
+          .then((result) => (symbol = result));
       }
     });
+  });
+
+  buttonMyBalance.addEventListener('click', async () => {
+    labelMyBalance.textContent = await window.profitDivider.methods
+      .balanceOf(window.accounts[0])
+      .call();
+    labelMyBalance.textContent += ' ' + symbol;
+  });
+
+  buttonContractOwner.addEventListener('click', async () => {
+    labelContractOwner.textContent = await window.profitDivider.methods.owner().call();
+  });
+
+  buttonContractBalanceOf.addEventListener('click', async () => {
+    if (!inputContractBalanceOf.value) return;
+    labelContractBalanceOf.textContent = await window.profitDivider.methods
+      .balanceOf(inputContractBalanceOf.value)
+      .call();
+    labelContractBalanceOf.textContent += ' ' + symbol;
   });
 };
 window.addEventListener('DOMContentLoaded', initialize);
